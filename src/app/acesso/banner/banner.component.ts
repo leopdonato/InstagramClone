2bdf13beb940d6d8db408bfe9abc60b36b7a1c73
Leonardo Donato <leo_donato007@yahoo.com.br>
2019-05-20 01:42:28 -0300

    Animacao homepage - transicao de imagens

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, state, style, animate, transition } from '@angular/animations';
+import { Imagem } from './imagem.model';
 
 @Component({
   selector: 'app-banner',
@@ -22,13 +23,42 @@ export class BannerComponent implements OnInit {
 
   public estado: string = 'visivel';
 
+  public imagens: Imagem[] = [
+    { estado: 'visivel', url: '/assets/banner-acesso/img_1.png'},
+    { estado: 'escondido', url: '/assets/banner-acesso/img_2.png'},
+    { estado: 'escondido', url: '/assets/banner-acesso/img_3.png'},
+    { estado: 'escondido', url: '/assets/banner-acesso/img_4.png'},
+    { estado: 'escondido', url: '/assets/banner-acesso/img_5.png'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => this.logicaRotacao(), 3000);
   }
 
-  public toggleEstado(): void {
-    this.estado = this.estado === 'visivel' ? 'escondido' : 'visivel';
+  public logicaRotacao(): void{
+
+    //auxilia na execucao da imagem seguint
+    let idx: number;
+
+    //ocultar imagem
+    for (let i: number = 0; i <= 4; i++ ) {
+
+      if (this.imagens[i].estado === 'visivel'){
+        this.imagens[i].estado = 'escondido';
+
+        idx = i === 4 ? 0 : i + 1;
+
+        break;
+      }
+
+    }
+
+    //exibir a proxima imagem
+    this.imagens[idx].estado = 'visivel';
+
+    setTimeout(() => this.logicaRotacao(), 3000);
   }
 
 }
