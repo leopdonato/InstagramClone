7d6388e6726bbd15b865fd2af1f81d4f0587a156
Leonardo Donato <leo_donato007@yahoo.com.br>
2019-05-20 17:21:43 -0300

    keyframes e callback

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-acesso',
@@ -27,7 +27,21 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
           opacity: 0,
           transform: 'translate( 50px, 0)'
         }),
-        animate('500ms 0s ease-in-out') //duracao, delay e aceleracao
+
+        //0 void -----x-------------x--x--x---------x- criado 1.5s//
+        animate('1.5s 0s ease-in-out', keyframes([
+          style({ offset: 0.15, opacity: 1, transform: 'translateX(0)'}),
+          style({ offset: 0.86, opacity: 1, transform: 'translateX(0)'}),
+
+          style({ offset: 0.88, opacity: 1, transform: 'translateY(-10px)'}),
+          style({ offset: 0.90, opacity: 1, transform: 'translateY(10px)'}),
+          style({ offset: 0.92, opacity: 1, transform: 'translateY(-10px)'}),
+          style({ offset: 0.94, opacity: 1, transform: 'translateY(10px)'}),
+          style({ offset: 0.96, opacity: 1, transform: 'translateY(-10px)'}),
+          style({ offset: 0.98, opacity: 1, transform: 'translateY(10px)'}),
+
+          style({ offset: 1, opacity: 1, transform: 'translateX(0)'})
+        ])) //duracao, delay e aceleracao
       ])
     ])
   ]
@@ -47,6 +61,14 @@ export class AcessoComponent implements OnInit {
 
   public exibirPainel(event: string): void{
     this.cadastro = event === 'cadastro' ? true : false;
+  }
+
+  public inicioDaAnimacao(): void{
+    console.log('inicio da animação');
+  }
+
+  public fimDaAnimacao(): void{
+    console.log('fim da animação');
   }
 
 }
