ea8fe282e9f056180bc72c900f6b485666e0bdaf
Leonardo Donato <leo_donato007@yahoo.com.br>
2019-05-20 01:13:00 -0300

    Inicializando animacoes no projeto

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  animations: [
+    trigger('banner', [
+      state('escondido', style({
+        opacity: 0
+      })),
+      state('visivel', style({
+        opacity: 1
+      })),
+      transition('escondido => visivel', animate('1s ease-in')),
+      transition('visivel => escondido', animate('1s ease-in'))
+    ])
+  ]
 })
 export class BannerComponent implements OnInit {
+
+  public estado: string = 'visivel';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public toggleEstado(): void {
+    this.estado = this.estado === 'visivel' ? 'escondido' : 'visivel';
   }
 
 }
